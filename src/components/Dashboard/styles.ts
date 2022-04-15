9a00865f7c22f3d2451e361ac8c517061d598e7c
Lucas Fouraux <lucasfouraux@gmail.com>
2022-04-15 19:34:57 -0300

    first commit

---
+++ src/components/Dashboard/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Container = styled.main` //serve para um elemento principal
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 2.5rem 1rem;
+`
